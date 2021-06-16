557adda3e39e93f312d75a3a16fb8780aac7eee9
Alisherov Sherali <dororosenpai312@gmail.com>
2021-06-16 16:26:55 +0600

    Hw2 done

---
+++ Hw2.py
@@ -0,0 +1,20 @@
+print("Введите число a=")
+a = int(input())
+print("Введите число b=")
+b = int(input())
+print("Выберите решение")
+print("1 - сложить a+b")
+print("2 - сложить a-b")
+d = int(input())
+
+
+if d == 1:
+    print("Сумма a+b=", a+b)
+if d == 2:
+    print("Разность a-b=",a-b)
+
+
+
+
+
+
